39d7765f40c6a5ffd6752ac29877193e54ed6b33
Aleksandra Mochol <mocholaleksandra@gmail.com>
2024-02-29 22:20:59 +0100

    new code

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import requests
 
 api = FastAPI()
 
-@api.get("/planets")
-def planets():
-    return "hello"
+@api.get("/planet")
+def planets(limit: int=10):
+    url = f"https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+top+{limit}+pl_name,hostname,sy_snum,sy_pnum,st_mass+from+ps&format=json"
+    r=requests.get(url)
+    return r.json()
+
+@api.get("/")
+def home():
+    return "Exoplanets API"
